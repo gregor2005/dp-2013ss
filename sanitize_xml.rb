cb208277f7a53528513ea1a49e01a63a5d77e8e4
gregor <gregor.binder@gmx.net>
2013-03-19 20:12:48 +0100

    sanitize_xml.rb created

---
+++ sanitize_xml.rb
@@ -0,0 +1,29 @@
+if ARGV.size != 2
+  puts "usage: ruby tool.rb <input file> <output file>"
+  exit
+end
+
+file=ARGV[0]
+output=ARGV[1]
+
+search_for="<element uid"
+filename="private_file"
+
+content=[]
+
+File.open(file,"r").each do |line|
+  if line.include?(search_for)
+    new_line = line.split('"')[0]
+    new_line << "\"#{filename}\""
+    new_line << line.split('"')[2..-1].join('"')
+    content << new_line
+  else
+    content << line
+  end
+end
+
+output_file = File.new(output,"w")
+
+content.each do |line|
+  output_file.puts(line)
+end
