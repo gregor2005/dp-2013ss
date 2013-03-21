d2b54c1ab2db8fb51a5d47d35a72c4cc85514338
gregor <gregor.binder@gmx.net>
2013-03-21 15:38:06 +0100

    sanitize_xml.rb: added "sample" clear property

--- sanitize_xml.rb
+++ sanitize_xml.rb
@@ -6,13 +6,14 @@ end
 file=ARGV[0]
 output=ARGV[1]
 
-search_for="<element uid"
+search_for_element="<element uid"
+search_for_sample="<sample uid"
 filename="private_file"
 
 content=[]
 
 File.open(file,"r").each do |line|
-  if line.include?(search_for)
+  if line.include?(search_for_element) || line.include?(search_for_sample)
     new_line = line.split('"')[0]
     new_line << "\"#{filename}\""
     new_line << line.split('"')[2..-1].join('"')
